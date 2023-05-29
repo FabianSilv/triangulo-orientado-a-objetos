22f4629ad51411b06e42efb765a316510d9ea3fb
FabianSilv <127145889+FabianSilv@users.noreply.github.com>
2023-05-28 22:07:44 -0300

    Add files via upload

---
+++ Triangulo-Orientado-a-Objetos/Triangulo Orientado a Objetos/Triangulos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo_Orientado_a_Objetos
+{
+    internal class Triangulos
+    {
+        public int ladoA;
+        public int ladoB;
+        public int ladoC;
+
+        public Triangulos(int ladoA, int ladoB, int ladoC)
+        {
+            this.ladoA = ladoA;
+            this.ladoB = ladoB;
+            this.ladoC = ladoC;
+        }
+
+        public string CalcularTriangulos()
+        {
+            if (ladoA + ladoB > ladoC && ladoA + ladoC > ladoB && ladoB + ladoC > ladoA)
+            {
+                 if (ladoA == ladoB && ladoA == ladoC)
+                   return "Equilatero";
+                else
+                    if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC)
+                    return "Isosceles";
+                else
+                    return "Escaleno";
+            }
+            else
+                return "Triangulo invalido!";
+        }
+    }
+}
